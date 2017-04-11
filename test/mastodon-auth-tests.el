88e7f432c089a31aea0694173fd16707ac01b931
Johnson Denen <johnson.denen@gmail.com>
2017-04-11 13:26:58 -0400

    Add mastodon--register-and-return-client-app test
    Refactor tests to check plist return value

--- test/mastodon-auth-tests.el
+++ test/mastodon-auth-tests.el
@@ -13,19 +13,32 @@
       (should (equal client-plist mastodon--client-app-plist)))))
 
 (ert-deftest mastodon-auth:register-client-app ()
-  "Should POST client data to /apps endpoint."
-  (with-mock
-    (mock (mastodon--api-for "apps") => "https://instance/api/v1/apps")
-    (mock (mastodon--http-post "https://instance/api/v1/apps"
-                               'mastodon--register-client-app-triage
-                               '(("client_name" . "mastodon.el")
-                                 ("redirect_uris" . "urn:ietf:wg:oauth:2.0:oob")
-                                 ("scopes" . "read write follow")
-                                 ("website" . "https://github.com/jdenen/mastodon.el"))))
-    (should (eq nil (mastodon--register-client-app)))))
+  "Should POST client data to /apps endpoint and return client plist."
+  (let ((app-plist '("id" "id-val" "secret" "secret-val")))
+    (with-mock
+      (mock (mastodon--api-for "apps") => "https://instance/api/v1/apps")
+      (mock (mastodon--register-client-app-triage "status") => app-plist)
+      (mock (mastodon--http-post "https://instance/api/v1/apps"
+                                 'mastodon--register-client-app-triage
+                                 '(("client_name" . "mastodon.el")
+                                   ("redirect_uris" . "urn:ietf:wg:oauth:2.0:oob")
+                                   ("scopes" . "read write follow")
+                                   ("website" . "https://github.com/jdenen/mastodon.el")))
+            => (funcall 'mastodon--register-client-app-triage "status"))
+      (should (eq app-plist (mastodon--register-client-app))))))
 
 (ert-deftest mastodon-auth:register-client-app-triage ()
-  "Should wrap `mastodon--http-response-triage' call."
-  (with-mock
-    (mock (mastodon--http-response-triage "status" 'mastodon-auth--registration-success))
-    (should (eq nil (mastodon--register-client-app-triage "status")))))
+  "Should wrap `mastodon--http-response-triage' call and return client plist."
+  (let ((app-plist '("id" "id-val" "secret" "secret-val")))
+    (with-mock
+      (mock (mastodon-auth--registration-success) => app-plist)
+      (mock (mastodon--http-response-triage "status" 'mastodon-auth--registration-success)
+            => (funcall 'mastodon-auth--registration-success))
+      (should (eq app-plist (mastodon--register-client-app-triage "status"))))))
+
+(ert-deftest mastodon-auth:register-and-return-client-app ()
+  "Should return a plist of client_id and client_secret after registration."
+  (let ((app-plist '("id" "id-val" "secret" "secret-val")))
+    (with-mock
+      (mock (mastodon--register-client-app) => app-plist)
+      (should (equal app-plist (mastodon--register-and-return-client-app))))))
