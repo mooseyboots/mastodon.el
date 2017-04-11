f091a811d0a6e13b22542caf08bd23833456f2c8
Johnson Denen <johnson.denen@gmail.com>
2017-04-11 12:56:23 -0400

    Add mastodon-auth--register-client-app test
    Add mastodon-auth--register-client-app-triage test

--- test/mastodon-auth-tests.el
+++ test/mastodon-auth-tests.el
@@ -2,6 +2,7 @@
 (load-file "../lisp/mastodon-auth.el")
 
 (ert-deftest mastodon-auth:registration-success ()
+  "Should set `mastodon--client-app-plist' on succesful registration."
   (let ((hash (make-hash-table :test 'equal))
         (client-plist '(:client_id "client-id" :client_secret "client-secret")))
     (puthash "client_id" "client-id" hash)
@@ -10,3 +11,21 @@
       (stub mastodon--json-hash-table => hash)
       (mastodon-auth--registration-success)
       (should (equal client-plist mastodon--client-app-plist)))))
+
+(ert-deftest mastodon-auth:register-client-app ()
+  "Should POST client data to /apps endpoint."
+  (with-mock
+    (mock (mastodon--api-for "apps") => "https://instance/api/v1/apps")
+    (mock (mastodon--http-post "https://instance/api/v1/apps"
+                               'mastodon--register-client-app-triage
+                               '(("client_name" . "mastodon.el")
+                                 ("redirect_uris" . "urn:ietf:wg:oauth:2.0:oob")
+                                 ("scopes" . "read write follow")
+                                 ("website" . "https://github.com/jdenen/mastodon.el"))))
+    (should (eq nil (mastodon--register-client-app)))))
+
+(ert-deftest mastodon-auth:register-client-app-triage ()
+  "Should wrap `mastodon--http-response-triage' call."
+  (with-mock
+    (mock (mastodon--http-response-triage "status" 'mastodon-auth--registration-success))
+    (should (eq nil (mastodon--register-client-app-triage "status")))))
