c6a4cfc846cd0ecd4eca7bdc02b0856d482bb364
Johnson Denen <johnson.denen@gmail.com>
2017-04-23 14:07:22 -0400

    Bump to v0.5.1

--- lisp/mastodon.el
+++ lisp/mastodon.el
@@ -2,7 +2,7 @@
 
 ;; Copyright (C) 2017 Johnson Denen
 ;; Author: Johnson Denen <johnson.denen@gmail.com>
-;; Version: 0.5.0
+;; Version: 0.5.1
 ;; Homepage: https://github.com/jdenen/mastodon.el
 
 ;; This file is not part of GNU Emacs.
@@ -37,7 +37,7 @@
   :prefix "mastodon-"
   :group 'external)
 
-(defconst mastodon-version "0.5.0"
+(defconst mastodon-version "0.5.1"
   "Current `mastodon' package version.")
 
 (defcustom mastodon-instance-url "https://mastodon.social"
