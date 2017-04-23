993fbec64edf128ba9e5a1aa61f93c385fe3be31
Johnson Denen <johnson.denen@gmail.com>
2017-04-23 01:14:18 -0400

    Clean up tests and update DOCSTRINGs

--- test/mastodon-tl-tests.el
+++ test/mastodon-tl-tests.el
@@ -1,15 +1,11 @@
 (require 'el-mock)
 
-(ert-deftest mastodon-tl:from-toot ()
-  "Should return the value for KEY in a list."
-  (should (string= (mastodon-tl--from-toot "foo" '(("foo" . "bar"))) "bar")))
+(ert-deftest remove-html-1 ()
+  "Should remove all <span> tags."
+  (let ((input "<span class=\"h-card\">foobar</span> <span>foobaz</span>"))
+    (should (string= (mastodon-tl--remove-html input) "foobar foobaz"))))
 
-(ert-deftest mastodon-tl:remove-html:remove-p-and-span ()
-  "Should remove <p> and <span> tags that are not parsed by `html2text'."
-  (let ((input "<p>foo<span>bar</span></p>"))
-    (should (string= (mastodon-tl--remove-html input) "foobar\n"))))
-
-(ert-deftest mastodon-tl:remove-html:remove-hcard-span ()
-  "Should remove <span> tags with a class of 'h-card'."
-  (let ((input "<span class=\"h-card\">foobar</span>"))
-    (should (string= (mastodon-tl--remove-html input) "foobar"))))
+(ert-deftest remove-html-2 ()
+  "Should replace <\p> tags with two new lines."
+  (let ((input "foobar</p>"))
+    (should (string= (mastodon-tl--remove-html input) "foobar\n\n"))))
