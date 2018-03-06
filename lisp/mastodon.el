d36be0b6ca57c219df45aa4af220c3e84dabe037
Alexander Griffith <griffitaj@gmail.com>
2018-03-05 20:39:17 -0500

    Bump version numbers to 0.7.2

--- lisp/mastodon.el
+++ lisp/mastodon.el
@@ -2,7 +2,7 @@
 
 ;; Copyright (C) 2017 Johnson Denen
 ;; Author: Johnson Denen <johnson.denen@gmail.com>
-;; Version: 0.7.1
+;; Version: 0.7.2
 ;; Package-Requires: ((emacs "24.4"))
 ;; Homepage: https://github.com/jdenen/mastodon.el
 
