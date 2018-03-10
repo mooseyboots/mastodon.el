0596fd337b66185b675c1555511efd74a38ab2ed
Alexander Griffith <griffitaj@gmail.com>
2018-03-10 13:13:25 -0500

    added mastodon-notifications--get to mastodon.el load in.

--- lisp/mastodon.el
+++ lisp/mastodon.el
@@ -48,6 +48,7 @@
 (autoload 'mastodon-toot--reply "mastodon-toot")
 (autoload 'mastodon-toot--toggle-boost "mastodon-toot")
 (autoload 'mastodon-toot--toggle-favourite "mastodon-toot")
+(autoload 'mastodon-notifications--get "mastodon-notifications")
 
 (defgroup mastodon nil
   "Interface with Mastodon."
