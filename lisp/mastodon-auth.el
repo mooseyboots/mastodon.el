5c33e9a108b3bbfab467707975e13b2b6124bec6
Johnson Denen <johnson.denen@gmail.com>
2018-08-10 17:21:33 -0400

    Bump to version 0.8.0

--- lisp/mastodon-auth.el
+++ lisp/mastodon-auth.el
@@ -2,7 +2,7 @@
 
 ;; Copyright (C) 2017 Johnson Denen
 ;; Author: Johnson Denen <johnson.denen@gmail.com>
-;; Version: 0.7.2
+;; Version: 0.8.0
 ;; Homepage: https://github.com/jdenen/mastodon.el
 ;; Package-Requires: ((emacs "24.4"))
 
