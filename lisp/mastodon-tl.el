6485f236ce9bab609a606d6f5896b1d39b3c114d
mousebot <mousebot@riseup.net>
2021-11-09 10:12:56 +0100

    fetch media_attachments' "type" from server and store as property
    - if the type is not "image", it is displayed in`'help-echo' property.
    
    - the idea is to use this to handle gifs/videos differently to images. but for
    now i'm not sure how to actually render such media. but this way, at least the
    item could be viewed externally if the user wants to see it, or at least they
    know they're missing out on something.
    
    - NB: EWW can't handle content type "video/mp4".

--- lisp/mastodon-tl.el
+++ lisp/mastodon-tl.el
@@ -676,10 +676,11 @@ message is a link which unhides/hides the main body."
                                  (if (alist-get 'remote_url media-attachement)
                                      (alist-get 'remote_url media-attachement)
                                    ;; fallback b/c notifications don't have remote_url
-                                   (alist-get 'url media-attachement))))
+                                   (alist-get 'url media-attachement)))
+                                (type (alist-get 'type media-attachement)))
                             (if mastodon-tl--display-media-p
                                 (mastodon-media--get-media-link-rendering
-                                 preview-url remote-url) ; 2nd arg for shr-browse-url
+                                 preview-url remote-url type) ; 2nd arg for shr-browse-url
                               (concat "Media::" preview-url "\n"))))
                         media-attachements "")))
     (if (not (and mastodon-tl--display-media-p
